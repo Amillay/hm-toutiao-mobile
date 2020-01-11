3ccfe660fb129028e87b69b4a64fe510bb69da5f
Amillay <1326747746@qq.com>
2020-01-11 11:10:36 +0800

    引入vant

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant'
+
+// 导入所有组件
+import 'vant/lib/index.css'
+Vue.use(Vant)
+// 完成注册在任意位置就可以使用vant组件库的组件   按需引用没必要在开发阶段引入
+
 Vue.config.productionTip = false
 
 new Vue({
