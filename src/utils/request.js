aae71608e4f0ebfbe3c499d64eee1d85ec6dfde7
Amillay <1326747746@qq.com>
2020-01-12 10:44:59 +0800

    处理token失效

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 import JSONBig from 'json-bigint'
 // 导入store
 import store from '@/store'
+// 引入路由对象实例
+import router from '@/router'
 
 // axios.defaults是对axios本身进行改造   现在是创建了一个新的和原来的没有关系了
 // 创建新的插件实例
@@ -40,8 +42,45 @@ instance.interceptors.response.use(function (response) {
   } catch (error) {
     return response.data
   }
-}, function (error) {
+}, async function (error) {
   // 错误的时候token容易失效具有失效问题
+//   如何判断失效401状态码
+  if (error.response && error.response.status === 401) {
+    // 表示token过期
+    // 获取当前页面地址  这里不能用this 组件实例
+    let toPath = { path: '/login', query: { redirectUrl: router.currentRoute.path } }// 跳转对象
+    // 先判断是否有refresh_token
+    if (store.state.user.refresh_token) {
+      // 应该发送一个请求换取一个新的token  使用instance这里会再次进入请求拦截器会使用原来的token
+      try {
+        let result = await axios({
+          method: 'put',
+          url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+          headers: {
+            Authorization: `Bearer ${store.state.user.refresh_token}`
+          }
+        })
+        store.commit('updateUser', { user: { token: result.data.data.token, refresh_token: store.state.user.refresh_token } })
+        // 更新了vuex数据也更新了本地缓存数据
+        return instance(error.config) // 把刚才错误的请求再次发送出去 必须要return将Promise返回
+      // result是返回的结果、
+        //   result.data.data.token
+      } catch (error) {
+        // 错误就是不就措施没有用了refresh_token也进行失效了
+        store.commit('delUser')
+        router.push(toPath) // 跳转登录
+      }
+    } else {
+      // 如果连refresh_token都没有  进行跳转
+      // 当访问页面是让你去登陆，登陆成功后  应该回到之前的页面
+      // 记住当前地址回到之前页面
+      // params 动态路由  user/:id   query 传参  user? id= 123
+
+      // router.currentRoute.path
+      // push除了可以是字符串还可以是一个对象router.push({path:'/login',query:{}})
+      router.push(toPath)
+    }
+  }
   return Promise.reject(error)
 })
 // 使用进行导出
